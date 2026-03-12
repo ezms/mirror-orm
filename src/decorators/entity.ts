4169465dd68fd6eed60e4f35c7bdecd3d9ef27e9
ezms <emanuelm.dev@gmail.com>
2026-03-12 11:11:24 -0300

    feat: change entity.ts so that the decorator's use can be: @Entity

--- src/decorators/entity.ts
+++ src/decorators/entity.ts
@@ -2,15 +2,33 @@ import { IColumnMetadata } from '../interfaces/column-metadata';
 import { registry } from '../metadata/registry';
 import { COLUMNS_KEY } from '../metadata/symbols';
 
-export const Entity = (tableName?: string) => {
-    return <T extends new (...args: Array<any>) => any>(_value: T, context: ClassDecoratorContext) => {
-        const className = String(context.name);
-        const columns = (context.metadata?.[COLUMNS_KEY] as Array<IColumnMetadata> | undefined) ?? [];
+type EntityDecorator = <T extends new (...args: Array<any>) => any>(value: T, context: ClassDecoratorContext) => void;
 
-        registry.registerEntity(className, {
-            tableName: tableName ?? className,
-            className,
-            columns,
-        });
-    };
+type EntityFactory = {
+    <T extends new (...args: Array<any>) => any>(value: T, context: ClassDecoratorContext): void;
+    (tableName: string): EntityDecorator;
+    (): EntityDecorator;
 };
+
+const applyEntity = (tableName: string | undefined, context: ClassDecoratorContext): void => {
+    const className = String(context.name);
+    const columns = (context.metadata?.[COLUMNS_KEY] as Array<IColumnMetadata> | undefined) ?? [];
+
+    registry.registerEntity(className, {
+        tableName: tableName ?? className,
+        className,
+        columns,
+    });
+};
+
+export const Entity = ((
+    arg?: string | (new (...args: Array<any>) => any),
+    context?: ClassDecoratorContext,
+): EntityDecorator | void => {
+    if (context) {
+        applyEntity(undefined, context);
+        return;
+    }
+    return (_value: new (...args: Array<any>) => any, ctx: ClassDecoratorContext) =>
+        applyEntity(arg as string | undefined, ctx);
+}) as EntityFactory;
